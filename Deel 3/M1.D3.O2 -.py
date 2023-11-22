af540bc11013adf5b8a6b77a405ae2a9b4a01366
Dimitri <99058899@mydavinci.nl>
2023-11-22 10:28:15 +0100

    Deel 3 opdracht2

---
+++ Deel 3/M1.D3.O2 -.py
@@ -0,0 +1,29 @@
+geel = input("Is de kaas geel? ").lower()
+if geel == "ja":
+    gaten = input("Zitten er gaten in? ").lower()
+    if gaten == "ja":
+        duur = input("Is de kaas belachelijk duur? ").lower()
+        if duur == "ja":
+            print("Emmenthaler")
+        else:
+            print("Leerdammer")
+    else:
+        hard = input("Is de kaas hard als steen? ").lower()
+        if hard == "ja":
+            print("Parmigiano Reggiano")
+        else:
+            print("Goudse kaas")
+else:
+    schimmel = input("Heeft de kaas blauwe schimmel? ").lower()
+    if schimmel == "ja":
+        korst1 = input("Heeft de kaas korst? ").lower()
+        if korst1 =='ja':
+            print("Bleu de Rochebaron")
+        else:
+            print("Foume d'ambert")
+    else:
+        korst2 = input("Heeft de kaas korst? ").lower()
+        if korst2 == "ja":
+            print("Camembert")
+        else:
+            print("Mozzarella")
