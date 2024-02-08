b467e06306e9674d91469c1d81034f9f2134c614
Dimitri <99058899@mydavinci.nl>
2024-02-08 09:28:47 +0100

    Deel 5

--- Deel 4/M1.D4.O4 - Exceptional Pizzacalculator.py
+++ Deel 4/M1.D4.O4 - Exceptional Pizzacalculator.py
@@ -6,29 +6,28 @@ try:
 
     if not all(A.is_integer() for A in [small, medium, large]):
         raise ValueError("Het aantal pizza's moet een geheel getal zijn.")
+    else:
+        #prijzen
+        SMALL_PRIJS = 6.50
+        MEDIUM_PRIJS = 8
+        LARGE_PRIJS = 11
 
-    #prijzen
-    SMALL_PRIJS = 6.50
-    MEDIUM_PRIJS = 8
-    LARGE_PRIJS = 11
+        #kosten
+        smallkosten = small * SMALL_PRIJS
+        mediumkosten = medium * MEDIUM_PRIJS
+        largekosten = large * LARGE_PRIJS
+        totaalprijs = smallkosten + mediumkosten + largekosten
 
-    #kosten
-    smallkosten = small * SMALL_PRIJS
-    mediumkosten = medium * MEDIUM_PRIJS
-    largekosten = large * LARGE_PRIJS
-    totaalprijs = smallkosten + mediumkosten + largekosten
-
-    #output
-    print(f"Uw bestelling: ")
-    print(f"{int(small)} kleine pizzas: €{smallkosten:.2f}")
-    print(f"{int(medium)} medium pizzas: €{mediumkosten:.2f}")
-    print(f"{int(large)} grote pizzas: €{largekosten:.2f}")
-    print(f"Uw totaalbedrag is €{totaalprijs:.2f}")
+        #output
+        print(f"Uw bestelling: ")
+        print(f"{int(small)} kleine pizzas: €{smallkosten:.2f}")
+        print(f"{int(medium)} medium pizzas: €{mediumkosten:.2f}")
+        print(f"{int(large)} grote pizzas: €{largekosten:.2f}")
+        print(f"Uw totaalbedrag is €{totaalprijs:.2f}")
 
 except ValueError as ve1:
     print(f"Foutieve invoer: {ve1}")
-except Exception as ve2:
-    print(f"Er is een fout opgetreden: {ve2}")
+
 
 
 
